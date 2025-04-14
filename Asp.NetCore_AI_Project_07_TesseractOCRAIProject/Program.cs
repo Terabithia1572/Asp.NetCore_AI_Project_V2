e2d3c4ad5a0335d1381c95d4d7e90d33f7ba98ae
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-04-14 22:09:33 +0300

    Writing OCR Codes Process Added

--- Asp.NetCore_AI_Project_07_TesseractOCRAIProject/Program.cs
+++ Asp.NetCore_AI_Project_07_TesseractOCRAIProject/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tesseract;
+
+static void Main(string[] args)
+{
+    Console.WriteLine("Karakter Okuması Yapılacak Resim Yolu:");
+    string imagePath = Console.ReadLine(); // Kullanıcıdan resim yolu alınıyor
+    Console.WriteLine("Tesseract Veri Yolu:");
+    string tessdataPath = @"C:\tessdata"; // Tesseract veri yolu
+
+    try
+    {
+        using (var engine = new TesseractEngine(tessdataPath, "eng", EngineMode.Default)) // Tesseract motoru başlatılıyor
+        {
+            using (var img = Pix.LoadFromFile(imagePath)) // Resim dosyası yükleniyor
+            {
+                using (var page = engine.Process(img)) //Resim İşleniyor
+                {
+                    string text = page.GetText(); // Resimden metin okunuyor
+                    Console.WriteLine("Okunan Metin:");
+                    Console.WriteLine(text); // Okunan metin ekrana yazdırılıyor
+                }
+            }
+        }
+    }
+    catch (Exception)
+    {
+
+       Console.WriteLine("Hata: Resim dosyası bulunamadı veya Tesseract veri yolu hatalı."); // Hata mesajı
+    }
+}
