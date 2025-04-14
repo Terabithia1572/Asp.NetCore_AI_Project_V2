d25c18bbc6772dde0bc2ce0ad8eee7e27ac2d11d
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-04-14 21:43:31 +0300

    DALL.E Image Generation Project Codes Added

--- Asp.NetCore_AI_Project_06_Dall-E_ImageGeneration/Program.cs
+++ Asp.NetCore_AI_Project_06_Dall-E_ImageGeneration/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+using System.Text;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        string apiKey= "api key burayagelecek"; // Open AI API key buraya gelecek
+        Console.WriteLine("DALL-E ile resim oluşturma işlemi başlatılıyor..."); // Başlangıç mesajı
+        string prompt=Console.ReadLine(); // Kullanıcıdan resim oluşturma isteği alınıyor
+        using (HttpClient client=new())
+        {
+            client.DefaultRequestHeaders.Add("Authorization",$"Bearer {apiKey}");
+            var requestBody = new
+            {
+                prompt = prompt,// Resim oluşturma isteği
+                n = 1,// Resim sayısı
+                size = "1024x1024" //Resim boyutu
+            };
+            string jsonBody=JsonConvert.SerializeObject(requestBody); // JSON formatına dönüştürülüyor
+            var content = new StringContent(jsonBody, Encoding.UTF8, "application/json"); // İçerik oluşturuluyor
+        }
+    }
+}
