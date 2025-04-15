bb2c2a33f4dffe19ac5ed767be1d1537c241ffca
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-04-15 21:19:00 +0300

    What is Google Cloud API Vision ?

---
+++ Asp.NetCore_AI_Project_08_GoogleCloudVision/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Google Cloud Vision API ile Karakter Okuması Yapılacak Resim Yolu:");
+        string imagePath = Console.ReadLine(); // Kullanıcıdan resim yolu alınıyor
+
+        string credentialPath = "buraya servis json dosyası gelecek";
+        Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credentialPath); // Google Cloud kimlik bilgileri ayarlanıyor
+        try
+        {
+            var client=ImageAnnotator
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+    }
+}
